7e2144377f43aea38e1a2f349fb962ce040bd3b1
Soni <soni@recalstudios.net>
2024-11-11 23:25:51 +0100

    feat: implement execution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 use std::process;
 use clap::Parser;
+use enigo::{Button, Coordinate, Enigo, Key, Keyboard, Mouse, Settings};
+use enigo::Direction::Click;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -15,22 +17,21 @@ struct Arguments {
 
 #[derive(Debug)]
 struct QueueItem {
-    time: u32,
+    time: u64,
     actions: Vec<Action>
 }
 
 #[derive(Debug)]
 enum Action {
     MouseMoveAction {
-        x: i16,
-        y: i16,
-        time: u16
+        x: i32,
+        y: i32
     },
     MousePressAction {
-        button: u8
+        button: Button
     },
     KeyPressAction {
-        key: char
+        key: Key
     }
 }
 
@@ -71,7 +72,7 @@ fn main() {
         // Decode line
         let line_timestamp_text = line_decoded[0];
         let line_actions_text = line_decoded[1];
-        let line_timestamp: u32 = line_timestamp_text.parse().unwrap_or_else(|error| {
+        let line_timestamp: u64 = line_timestamp_text.parse().unwrap_or_else(|error| {
             println!("Line {line_index}: Incorrectly formatted timestamp: {line_timestamp_text:?} ({error})");
             process::exit(1);
         });
@@ -103,39 +104,28 @@ fn main() {
                 // Validate arguments
                 let segments: Vec<&str> = action.split_whitespace().collect();
                 if segments.len() < 3 {
-                    println!("Line {line_index} (mousemove): Need at least 2 arguments");
+                    println!("Line {line_index} (mousemove): Too few arguments! (min. 2 arguments)");
                     process::exit(1);
                 }
-                if segments.len() > 4 {
-                    println!("Line {line_index} (mousemove): Too many arguments provided (max 3 arguments)");
+                if segments.len() > 3 {
+                    println!("Line {line_index} (mousemove): Too many arguments provided (max. 2 arguments)");
                     process::exit(1);
                 }
 
                 // Parse X position
-                let x: i16 = segments[1].parse().unwrap_or_else(|error| {
+                let x: i32 = segments[1].parse().unwrap_or_else(|error| {
                     println!("Line {line_index} (mousemove): Invalid X position {:?} ({error})", segments[1]);
                     process::exit(1);
                 });
 
                 // Parse Y position
-                let y: i16 = segments[2].parse().unwrap_or_else(|error| {
+                let y: i32 = segments[2].parse().unwrap_or_else(|error| {
                     println!("Line {line_index} (mousemove): Invalid Y position {:?} ({error})", segments[2]);
                     process::exit(1);
                 });
 
-                // Parse time
-                let time: u16;
-                if segments.len() == 4 {
-                    time = segments[3].parse().unwrap_or_else(|error| {
-                        println!("Line {line_index} (mousemove): Invalid time {:?} ({error})", segments[3]);
-                        process::exit(1);
-                    });
-                } else {
-                    time = 0;
-                }
-
                 // Add to actions
-                actions.push(Action::MouseMoveAction { x, y, time });
+                actions.push(Action::MouseMoveAction { x, y });
             } else if action.starts_with("mousepress") {
                 // Validate arguments
                 let segments: Vec<&str> = action.split_whitespace().collect();
@@ -144,15 +134,27 @@ fn main() {
                     process::exit(1);
                 }
                 if segments.len() > 2 {
-                    println!("Line {line_index} (mousepress): Too many arguments provided (max 1 argument)");
+                    println!("Line {line_index} (mousepress): Too many arguments provided (max. 1 argument)");
                     process::exit(1);
                 }
 
                 // Parse button
-                let button: u8 = segments[1].parse().unwrap_or_else(|error| {
+                let button_number: u8 = segments[1].parse().unwrap_or_else(|error| {
                     println!("Line {line_index} (mousepress): Invalid button {:?} ({error})", segments[1]);
                     process::exit(1);
                 });
+
+                let button = match button_number {
+                    1 => Button::Left,
+                    2 => Button::Right,
+                    3 => Button::Middle,
+                    4 => Button::Back,
+                    5 => Button::Forward,
+                    _ => {
+                        println!("Line {line_index} (mousepress): Invalid button {:?}", segments[1]);
+                        process::exit(1);
+                    }
+                };
 
                 // Add to actions
                 actions.push(Action::MousePressAction { button });
@@ -164,15 +166,81 @@ fn main() {
                     process::exit(1);
                 }
                 if segments.len() > 2 {
-                    println!("Line {line_index} (keypress): Too many arguments provided (max 1 argument)");
+                    println!("Line {line_index} (keypress): Too many arguments provided (max. 1 argument)");
                     process::exit(1);
                 }
 
                 // Parse key
-                let key: char = segments[1].parse().unwrap_or_else(|error| {
-                    println!("Line {line_index} (keypress): Invalid key {:?} ({error})", segments[1]);
-                    process::exit(1);
-                });
+                let key = match segments[1].to_lowercase().as_str() {
+                    "alt" => Key::Alt,
+                    "backspace" => Key::Backspace,
+                    "capslock" => Key::CapsLock,
+                    "control" => Key::Control,
+                    "delete" => Key::Delete,
+                    "down" => Key::DownArrow,
+                    "end" => Key::End,
+                    "enter" => Key::Return,
+                    "escape" => Key::Escape,
+                    "f1" => Key::F1,
+                    "f2" => Key::F2,
+                    "f3" => Key::F3,
+                    "f4" => Key::F4,
+                    "f5" => Key::F5,
+                    "f6" => Key::F6,
+                    "f7" => Key::F7,
+                    "f8" => Key::F8,
+                    "f9" => Key::F9,
+                    "f10" => Key::F10,
+                    "f11" => Key::F11,
+                    "f12" => Key::F12,
+                    "f13" => Key::F13,
+                    "f14" => Key::F14,
+                    "f15" => Key::F15,
+                    "f16" => Key::F16,
+                    "f17" => Key::F17,
+                    "f18" => Key::F18,
+                    "f19" => Key::F19,
+                    "f20" => Key::F20,
+                    "home" => Key::Home,
+                    "insert" => Key::Insert,
+                    "left" => Key::LeftArrow,
+                    "pagedown" => Key::PageDown,
+                    "pageup" => Key::PageUp,
+                    "right" => Key::RightArrow,
+                    "shift" => Key::Shift,
+                    "space" => Key::Space,
+                    "super" => Key::Meta,
+                    "tab" => Key::Tab,
+                    "up" => Key::UpArrow,
+                    _ => {
+                        // Parse non-special keys
+                        let key: char = segments[1].to_lowercase().parse().unwrap_or_else(|error| {
+                            println!("Line {line_index} (keypress): Invalid key {:?} ({error})", segments[1]);
+                            process::exit(1);
+                        });
+
+                        // Disallow non-standard keys
+                        match key {
+                            'a'..='z' => Key::Unicode(key),
+                            '0'..='9' => Key::Unicode(key),
+                            '`' => Key::Unicode(key),
+                            '-' => Key::Unicode(key),
+                            '=' => Key::Unicode(key),
+                            '[' => Key::Unicode(key),
+                            ']' => Key::Unicode(key),
+                            '\\' => Key::Unicode(key),
+                            ';' => Key::Unicode(key),
+                            '\'' => Key::Unicode(key),
+                            ',' => Key::Unicode(key),
+                            '.' => Key::Unicode(key),
+                            '/' => Key::Unicode(key),
+                            _ => {
+                                println!("Line {line_index} (keypress): Invalid key {:?}", segments[1]);
+                                process::exit(1);
+                            }
+                        }
+                    }
+                };
 
                 // Add to actions
                 actions.push(Action::KeyPressAction { key });
@@ -189,5 +257,36 @@ fn main() {
         });
     }
 
-    println!("{:?}", queue);
+    // Execute file
+    let mut current_timestamp = 0;
+    let mut enigo = Enigo::new(&Settings::default()).unwrap_or_else(|error| {
+        println!("Failed to initialize Enigo: {error}");
+        process::exit(1);
+    });
+
+    for entry in queue {
+        // Calculate wait time
+        let wait_time = entry.time - current_timestamp;
+        std::thread::sleep(std::time::Duration::from_millis(wait_time));
+
+        // Execute actions
+        for action in entry.actions {
+            match action {
+                Action::MouseMoveAction { x, y } => {
+                    let _ = enigo.move_mouse(x, y, Coordinate::Abs);
+                }
+                Action::MousePressAction { button } => {
+                    let _ = enigo.button(button, Click);
+                }
+                Action::KeyPressAction { key } => {
+                    if let Err(error) = enigo.key(key, Click) {
+                        println!("Failed to press key {key:?}: {error}");
+                    }
+                }
+            }
+        }
+
+        // Update current timestamp
+        current_timestamp = entry.time;
+    }
 }
