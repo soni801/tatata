5c4790e095e521ae7d1370313a720dd4f666102c
Soni <soni@recalstudios.net>
2024-11-11 19:09:52 +0100

    feat: parse actions

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,17 @@ struct QueueItem {
 
 #[derive(Debug)]
 enum Action {
-    MouseMoveAction,
-    MousePressAction,
-    KeyPressAction
+    MouseMoveAction {
+        x: i16,
+        y: i16,
+        time: u16
+    },
+    MousePressAction {
+        button: u8
+    },
+    KeyPressAction {
+        key: char
+    }
 }
 
 fn main() {
@@ -64,7 +72,7 @@ fn main() {
         let line_timestamp_text = line_decoded[0];
         let line_actions_text = line_decoded[1];
         let line_timestamp: u32 = line_timestamp_text.parse().unwrap_or_else(|error| {
-            println!("Line {line_index}: Incorrectly formatted timestamp: \"{line_timestamp_text}\" ({error})");
+            println!("Line {line_index}: Incorrectly formatted timestamp: {line_timestamp_text:?} ({error})");
             process::exit(1);
         });
         let line_actions: Vec<&str> = line_actions_text.split(";").collect();
@@ -92,11 +100,82 @@ fn main() {
 
             // Add Action to actions
             if action.starts_with("mousemove") {
-                actions.push(Action::MouseMoveAction);
+                // Validate arguments
+                let segments: Vec<&str> = action.split_whitespace().collect();
+                if segments.len() < 3 {
+                    println!("Line {line_index} (mousemove): Need at least 2 arguments");
+                    process::exit(1);
+                }
+                if segments.len() > 4 {
+                    println!("Line {line_index} (mousemove): Too many arguments provided (max 3 arguments)");
+                    process::exit(1);
+                }
+
+                // Parse X position
+                let x: i16 = segments[1].parse().unwrap_or_else(|error| {
+                    println!("Line {line_index} (mousemove): Invalid X position {:?} ({error})", segments[1]);
+                    process::exit(1);
+                });
+
+                // Parse Y position
+                let y: i16 = segments[2].parse().unwrap_or_else(|error| {
+                    println!("Line {line_index} (mousemove): Invalid Y position {:?} ({error})", segments[2]);
+                    process::exit(1);
+                });
+
+                // Parse time
+                let time: u16;
+                if segments.len() == 4 {
+                    time = segments[3].parse().unwrap_or_else(|error| {
+                        println!("Line {line_index} (mousemove): Invalid time {:?} ({error})", segments[3]);
+                        process::exit(1);
+                    });
+                } else {
+                    time = 0;
+                }
+
+                // Add to actions
+                actions.push(Action::MouseMoveAction { x, y, time });
             } else if action.starts_with("mousepress") {
-                actions.push(Action::MousePressAction);
+                // Validate arguments
+                let segments: Vec<&str> = action.split_whitespace().collect();
+                if segments.len() < 2 {
+                    println!("Line {line_index} (mousepress): No argument provided");
+                    process::exit(1);
+                }
+                if segments.len() > 2 {
+                    println!("Line {line_index} (mousepress): Too many arguments provided (max 1 argument)");
+                    process::exit(1);
+                }
+
+                // Parse button
+                let button: u8 = segments[1].parse().unwrap_or_else(|error| {
+                    println!("Line {line_index} (mousepress): Invalid button {:?} ({error})", segments[1]);
+                    process::exit(1);
+                });
+
+                // Add to actions
+                actions.push(Action::MousePressAction { button });
             } else if action.starts_with("keypress") {
-                actions.push(Action::KeyPressAction);
+                // Validate arguments
+                let segments: Vec<&str> = action.split_whitespace().collect();
+                if segments.len() < 2 {
+                    println!("Line {line_index} (keypress): No argument provided");
+                    process::exit(1);
+                }
+                if segments.len() > 2 {
+                    println!("Line {line_index} (keypress): Too many arguments provided (max 1 argument)");
+                    process::exit(1);
+                }
+
+                // Parse key
+                let key: char = segments[1].parse().unwrap_or_else(|error| {
+                    println!("Line {line_index} (keypress): Invalid key {:?} ({error})", segments[1]);
+                    process::exit(1);
+                });
+
+                // Add to actions
+                actions.push(Action::KeyPressAction { key });
             } else {
                 println!("Line {line_index}: Invalid action: {action:?}");
                 process::exit(1);
